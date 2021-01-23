949195ece26a15dd111c20da614ab1053e09a05f
Leah Riffell <leah.riffell@gmail.com>
2021-01-23 08:11:24 -0700

    Add iteration 2

---
+++ test/potluck_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+require './lib/potluck'
+
+class PotluckTest <  Minitest::Test
+  def setup
+    @potluck = Potluck.new('7-13-18')
+    @couscous_salad = Dish.new('Couscous Salad', :appetizer)
+    @cocktail_meatballs = Dish.new('Cocktail Meatballs', :entre)
+  end
+
+  def test_it_exists
+    assert_instance_of Potluck, @potluck
+  end
+
+  def test_it_has_a_date
+    assert_equal '7-13-18', @potluck.date
+  end
+
+  def test_it_has_dishes
+    assert_equal [], @potluck.dishes
+  end
+
+  def test_it_can_add_dishes
+    assert_equal [], @potluck.dishes
+
+    @potluck.add_dish(@couscous_salad)
+    assert_equal [@couscous_salad], @potluck.dishes
+
+    @potluck.add_dish(@cocktail_meatballs)
+    assert_equal [@couscous_salad, @cocktail_meatballs], @potluck.dishes
+
+    assert_equal 2, @potluck.dishes.length
+  end
+end

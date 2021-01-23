dd44a2e50128d3e4f79c45438b821b5c36fa20e7
Leah Riffell <leah.riffell@gmail.com>
2021-01-23 11:38:38 -0700

    Add iteration 4

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,3 +1,5 @@
+require 'active_support/inflector'
+
 class Potluck
   attr_accessor :date,
                 :dishes
@@ -13,5 +15,18 @@ class Potluck
   
   def get_all_from_category(category)
     @dishes.find_all { |dish| dish.category == category }
+  end
+
+  def menu
+    menu = Hash.new([])
+    [:appetizers, :entrees, :desserts].each do |course|
+      menu[course] = @dishes.map { |dish| dish.name if dish.category == course[0..-2].to_sym }.compact.sort
+    end
+    menu
+  end
+
+  def ratio(category)
+    curated_menu ||= menu
+    (curated_menu[category.to_s.pluralize.to_sym].size / curated_menu.values.flatten.size.to_f) * 100
   end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -7,7 +7,11 @@ class PotluckTest <  Minitest::Test
   def setup
     @potluck = Potluck.new('7-13-18')
     @couscous_salad = Dish.new('Couscous Salad', :appetizer)
-    @cocktail_meatballs = Dish.new('Cocktail Meatballs', :entre)
+    @cocktail_meatballs = Dish.new('Cocktail Meatballs', :entree)
+    @summer_pizza = Dish.new('Summer Pizza', :appetizer)
+    @roast_pork = Dish.new('Roast Pork', :entree)
+    @candy_salad = Dish.new('Candy Salad', :dessert)
+    @bean_dip = Dish.new("Bean Dip", :appetizer)
   end
 
   def test_it_exists
@@ -35,18 +39,37 @@ class PotluckTest <  Minitest::Test
   end
 
   def test_it_can_get_dishes_from_a_category
-    summer_pizza = Dish.new('Summer Pizza', :appetizer)
-    roast_pork = Dish.new('Roast Pork', :entre)
-    candy_salad = Dish.new('Candy Salad', :dessert)
-
     @potluck.add_dish(@couscous_salad)
-    @potluck.add_dish(summer_pizza)
-    @potluck.add_dish(roast_pork)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
     @potluck.add_dish(@cocktail_meatballs)
-    @potluck.add_dish(candy_salad)
+    @potluck.add_dish(@candy_salad)
 
-    assert_equal [@couscous_salad, summer_pizza], @potluck.get_all_from_category(:appetizer)
+    assert_equal [@couscous_salad, @summer_pizza], @potluck.get_all_from_category(:appetizer)
     assert_equal @couscous_salad, @potluck.get_all_from_category(:appetizer).first
     assert_equal 'Couscous Salad', @potluck.get_all_from_category(:appetizer).first.name
+  end
+
+  def test_it_can_sort_dishes_alphabetically_by_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
+
+    assert_equal ({ :appetizers => ['Bean Dip', 'Couscous Salad', 'Summer Pizza'], :entrees => ['Cocktail Meatballs', 'Roast Pork'], :desserts => ['Candy Salad'] }), @potluck.menu
+  end
+
+  def test_it_can_get_the_ratio_of_dishes_in_a_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
+
+    assert_equal 50.0, @potluck.ratio(:appetizer)
+    assert_equal 0, @potluck.ratio(:snack)
   end
 end

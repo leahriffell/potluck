a40854fc57764e883908d02c13301b868d61b035
Leah Riffell <leah.riffell@gmail.com>
2021-01-23 07:58:40 -0700

    Add iteration 1

---
+++ test/dish_test.rb
@@ -0,0 +1,21 @@
+require './lib/dish'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class DishTest <  Minitest::Test
+  def setup
+    @dish = Dish.new('Couscous Salad', :appetizer)
+  end
+
+  def test_it_exists
+    assert_instance_of Dish, @dish
+  end
+
+  def test_it_has_a_name
+    assert_equal 'Couscous Salad', @dish.name
+  end
+
+  def test_it_has_a_category
+    assert_equal :appetizer, @dish.category
+  end
+end
